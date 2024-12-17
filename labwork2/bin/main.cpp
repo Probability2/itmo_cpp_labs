31defafd99fb9c60f8ae3d52f4e699e556774288
Michael Balakirsky <65661353+Probability2@users.noreply.github.com>
2024-12-17 21:50:00 +0200

    Create main.cpp

---
+++ labwork2/bin/main.cpp
@@ -0,0 +1,10 @@
+#include "../lib/number.cpp"
+#include <iostream>
+
+int main() {
+    int2023_t value1;
+    int2023_t value2;
+    int2023_t value3;
+    value1 = from_string("239");
+    return 0;
+}
